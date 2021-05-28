0f4328b7ed60c7eec3238f2cfa0ae541484271bb
metadotmy <metadotmy@gmail.com>
2021-05-28 20:33:13 +0800

    Trigger on privmsg only.

--- Premades/Trigger-Animation/TA-JS.js
+++ Premades/Trigger-Animation/TA-JS.js
@@ -12,6 +12,37 @@ document.addEventListener('onEventReceived', function(obj) {
     if (obj.detail.command === "PRIVMSG") { // Prevent animation for ping events - Curtis Geiger
         $('#log>div').last().hide().slideToggle(600, "easeInOutQuart"); //New animation code
         $("#custom_html").append("<div class='heart animate-up'></div>");
+
+        /* Script for animation by remi */
+        var b = Math.floor(Math.random() * 100 + 1);
+        var d = ["flowOne", "flowTwo", "flowThree"];
+        var a = [
+            "colOne",
+            "colTwo",
+            "colThree",
+            "colFour",
+            "colFive",
+            "colSix",
+        ];
+        var c = (Math.random() * (5 - 2) + 1.2).toFixed(1); //1.6 - 1.2 ori value.
+        $(
+            '<div class="heart part-' +
+            b +
+            " " +
+            a[Math.floor(Math.random() * 6)] +
+            '" style="font-size:' +
+            Math.floor(Math.random() * (50 - 22) + 22) +
+            'px;"><i class="fa fa-heart"></i></div>'
+        )
+            .appendTo(".hearts")
+            .css({
+            animation:
+                "" + d[Math.floor(Math.random() * 3)] + " " + c + "s linear",
+            }); //
+        $(".part-" + b).show();
+        setTimeout(function () {
+            $(".part-" + b).remove();
+        }, c * 900);
     }
   
     /* Old animation code - idk which is better tbh.
@@ -27,34 +58,5 @@ document.addEventListener('onEventReceived', function(obj) {
 
     */
 
-    /* Script for animation by remi */
-    var b = Math.floor(Math.random() * 100 + 1);
-    var d = ["flowOne", "flowTwo", "flowThree"];
-    var a = [
-        "colOne",
-        "colTwo",
-        "colThree",
-        "colFour",
-        "colFive",
-        "colSix",
-    ];
-    var c = (Math.random() * (5 - 2) + 1.2).toFixed(1); //1.6 - 1.2 ori value.
-    $(
-        '<div class="heart part-' +
-        b +
-        " " +
-        a[Math.floor(Math.random() * 6)] +
-        '" style="font-size:' +
-        Math.floor(Math.random() * (50 - 22) + 22) +
-        'px;"><i class="fa fa-heart"></i></div>'
-    )
-        .appendTo(".hearts")
-        .css({
-        animation:
-            "" + d[Math.floor(Math.random() * 3)] + " " + c + "s linear",
-        }); //
-    $(".part-" + b).show();
-    setTimeout(function () {
-        $(".part-" + b).remove();
-    }, c * 900);
+    
 });
--- Premades/Trigger-Animation/readme.txt
+++ Premades/Trigger-Animation/readme.txt
@@ -1,3 +1,3 @@
-Example of animation triggered on message received.
+Example of animation triggered on message received. From testing, animation can break if too many messages received at once. not sure why.
 
-Heart animation by remi - https://codepen.io/remsrob/pen/zjVQGx 
+Instalive Heart animation by remi - https://codepen.io/remsrob/pen/zjVQGx 
