a56f6f27317550a1c0a28429ecfefff3d902b0a1
metadotmy <metadotmy@gmail.com>
2021-09-23 02:18:29 +0800

    Added Whitebox theme.

---
+++ project/Themes/05 - White Box/Whitebox.js
@@ -0,0 +1,29 @@
+// Please use event listeners to run functions.
+document.addEventListener('onLoad', function(obj) {
+    // obj will be empty for chat widget
+    // this will fire only once when the widget loads
+});
+
+document.addEventListener('onEventReceived', function (obj) {
+    // obj will contain information about the event
+    console.log(obj.detail); // OBJ Logs
+
+    const smoothscroll = {smoothscroll};
+    const limitEnable = {limit_enable};
+    const msgLimit = {msg_limit};
+    const msgParent = document.querySelector('.sl__chat__layout');
+
+    if (obj.detail.command === "PRIVMSG") {
+        if (smoothscroll == true) {
+            $('#log>div').last().hide().slideToggle(600, "easeInOutQuart");
+        }
+        if (limitEnable == true) {
+            if (msgParent.children.length > msgLimit) {
+                $('#log>div').not($('#log>div').slice(-msgLimit)).fadeOut();
+            }
+        }
+    }
+    if (obj.detail.command === "CLEARCHAT") {
+        $('#log').empty();
+    }
+});
