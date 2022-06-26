8f0fd5cd81d87041c6d0985037f910f5d854d9b8
metadotmy <metadotmy@gmail.com>
2022-06-26 13:17:05 +0800

    Extension for basic chatbox

---
+++ Streamlabs/Basic/Default-JS.js
@@ -0,0 +1,30 @@
+// Please use event listeners to run functions.
+document.addEventListener('onLoad', function(obj) {
+    // obj will be empty for chat widget
+    // this will fire only once when the widget loads
+});
+
+document.addEventListener('onEventReceived', function (obj) {
+    // obj will contain information about the event
+    console.log(obj.detail); // OBJ Logs
+
+    const smoothscroll = {smoothscroll};
+    const limitEnable = {enable_limit};
+    const msgLimit = {message_limit};
+    const msgParent = document.querySelector('.sl__chat__layout');
+
+    if (obj.detail.command === "PRIVMSG") {
+        if (smoothscroll == true) {
+            $('#log>div').last().hide().slideToggle(600, "easeInOutQuart");
+        }
+        if (limitEnable == true) {
+            if (msgParent.children.length > msgLimit) {
+                $('#log>div').not($('#log>div').slice(-msgLimit)).fadeOut();
+            }
+        }
+    }
+    // clear whole chat but not /timeout /ban
+    if (obj.detail.command === "CLEARCHAT" && typeof obj.detail.body === "undefined") {
+        $('#log').empty();
+    }
+});
---
+++ Streamlabs/Basic/Extend/Extend.js
@@ -0,0 +1,16 @@
+/*    /announce support for Streamlabs.
+
+$msg-param-color only available if send thru twitch's site. does not exist from chatterino.
+
+
+*/
+
+if (obj.detail.command === "USERNOTICE" && obj.detail.tags["msg-id"] === "announcement") {
+    console.log(obj.detail);
+    let message_body = obj.detail.body;
+    let user_name = obj.detail.tags["display-name"];
+    let announcement_color = obj.detail.tags["msg-param-color"];
+    //append to chatbox - no emote support as sl parse those.
+    // idk maybe create newevent, convert to privmsg. might work.
+}
+
