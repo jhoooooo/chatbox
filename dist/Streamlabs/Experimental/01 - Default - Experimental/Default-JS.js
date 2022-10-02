853b00f83f86fa057dc8d1b35aa47e17a048b043
jhoooooo <metadotmy@gmail.com>
2022-10-02 15:55:19 +0800

    added experimental appending replies into chat

---
+++ dist/Streamlabs/Experimental/01 - Default - Experimental/Default-JS.js
@@ -0,0 +1,53 @@
+// Please use event listeners to run functions.
+document.addEventListener('onLoad', function(obj) {
+    // obj will be empty for chat widget
+    // this will fire only once when the widget loads
+});
+
+document.addEventListener('onEventReceived', function (obj) {
+    // obj will contain information about the event
+    console.log(obj.detail); // OBJ Logs
+
+    const smoothscroll = {smoothscroll};
+    const limitEnable = {enable_limit};
+    const msgLimit = {message_limit};
+    const msgParent = document.querySelector('.sl__chat__layout');
+
+    if (obj.detail.command === "PRIVMSG") {
+        if (smoothscroll == true) {
+            $('#log>div').last().hide().slideToggle(600, "easeInOutQuart");
+        }
+        if (limitEnable == true) {
+            if (msgParent.children.length > msgLimit) {
+                $('#log>div').not($('#log>div').slice(-msgLimit)).fadeOut();
+            }
+        }
+
+        // Experiment Starts - append reply parent
+        if (typeof obj.detail.tags["reply-parent-msg-body"] != "undefined") {
+            let parent_msg = obj.detail.tags["reply-parent-msg-body"];
+            let new_parent_msg = parent_msg.replaceAll('\\\s', ' ')
+            reply_to = obj.detail.tags["reply-parent-display-name"];
+            last_elem = $('#log>div>div').last();
+            last_elem.before(`<div class='reply_to'><?xml version="1.0" encoding="UTF-8"?>
+            <svg width="24" fill="currentColor" viewBox="-2 -2.5 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 0h14a3 3 0 0 1 3 3v8a3 3 0 0 1-3 3h-6.958l-6.444 4.808A1 1 0 0 1 2 18.006V14a2 2 0 0 1-2-2V3a3 3 0 0 1 3-3zm6.378 12H17a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1 1 1 0 0 1 1 1v3.013L9.378 12zM13 9a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM7 9a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/>
+            </svg> ${reply_to} : ${new_parent_msg} </div>`);
+
+            /*
+            "reply-parent-display-name": "jhooooooooobot",
+            "reply-parent-msg-body": "lorem\\sipsum",
+            "reply-parent-msg-id": "c1ef3be6-0f54-4531-ac1f-f03ddd4f4b06",
+            "reply-parent-user-id": "674193841",
+            "reply-parent-user-login": "jhooooooooobot",
+            */
+
+        }
+
+
+    }
+    // clear whole chat but not /timeout /ban
+    if (obj.detail.command === "CLEARCHAT" && typeof obj.detail.body === "undefined") {
+        $('#log').empty();
+    }
+});
