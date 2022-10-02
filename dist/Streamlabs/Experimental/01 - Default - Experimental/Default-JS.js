27a3e685d7df18b68a20daba2e8299a14c297ae6
jhoooooo <metadotmy@gmail.com>
2022-10-02 17:08:54 +0800

    added styling for reply append

--- dist/Streamlabs/Experimental/01 - Default - Experimental/Default-JS.js
+++ dist/Streamlabs/Experimental/01 - Default - Experimental/Default-JS.js
@@ -29,10 +29,12 @@ document.addEventListener('onEventReceived', function (obj) {
             let new_parent_msg = parent_msg.replaceAll('\\\s', ' ')
             reply_to = obj.detail.tags["reply-parent-display-name"];
             last_elem = $('#log>div>div').last();
-            last_elem.before(`<div class='reply_to'><?xml version="1.0" encoding="UTF-8"?>
-            <svg width="24" fill="currentColor" viewBox="-2 -2.5 24 24" xmlns="http://www.w3.org/2000/svg">
+            last_elem.before(`<div class='reply_to message'><?xml version="1.0" encoding="UTF-8"?>
+            <svg width="1em" fill="currentColor" viewBox="-2 -2.5 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 0h14a3 3 0 0 1 3 3v8a3 3 0 0 1-3 3h-6.958l-6.444 4.808A1 1 0 0 1 2 18.006V14a2 2 0 0 1-2-2V3a3 3 0 0 1 3-3zm6.378 12H17a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1 1 1 0 0 1 1 1v3.013L9.378 12zM13 9a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM7 9a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/>
-            </svg> ${reply_to} : ${new_parent_msg} </div>`);
+            </svg> replying to @${reply_to} : ${new_parent_msg} </div>`);
+
+            $('.wrapper:has(.reply_to)').addClass('border');
 
             /*
             "reply-parent-display-name": "jhooooooooobot",
