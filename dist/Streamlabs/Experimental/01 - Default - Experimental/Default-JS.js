90f07b79b066bc887537d56cba1bd46fb961a658
jhoooooo <metadotmy@gmail.com>
2022-10-02 18:43:04 +0800

    further improve styling

--- dist/Streamlabs/Experimental/01 - Default - Experimental/Default-JS.js
+++ dist/Streamlabs/Experimental/01 - Default - Experimental/Default-JS.js
@@ -27,12 +27,13 @@ document.addEventListener('onEventReceived', function (obj) {
         if (typeof obj.detail.tags["reply-parent-msg-body"] != "undefined") {
             let parent_msg = obj.detail.tags["reply-parent-msg-body"];
             let new_parent_msg = parent_msg.replaceAll('\\\s', ' ')
+            let truncated_parent_msg = new_parent_msg.substring(0,30) + "...";
             reply_to = obj.detail.tags["reply-parent-display-name"];
             last_elem = $('#log>div>div').last();
             last_elem.before(`<div class='reply_to message'><?xml version="1.0" encoding="UTF-8"?>
-            <svg width="1em" fill="currentColor" viewBox="-2 -2.5 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg class="reply_svg" width="1em" fill="currentColor" viewBox="-2 -2.5 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 0h14a3 3 0 0 1 3 3v8a3 3 0 0 1-3 3h-6.958l-6.444 4.808A1 1 0 0 1 2 18.006V14a2 2 0 0 1-2-2V3a3 3 0 0 1 3-3zm6.378 12H17a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1 1 1 0 0 1 1 1v3.013L9.378 12zM13 9a2 2 0 1 1 0-4 2 2 0 0 1 0 4zM7 9a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/>
-            </svg> replying to @${reply_to} : ${new_parent_msg} </div>`);
+            </svg> replying to @${reply_to} : ${truncated_parent_msg} </div>`);
 
             $('.wrapper:has(.reply_to)').addClass('border');
 
@@ -43,6 +44,8 @@ document.addEventListener('onEventReceived', function (obj) {
             "reply-parent-user-id": "674193841",
             "reply-parent-user-login": "jhooooooooobot",
             */
+
+            /* todo: appnd parent msg-id for deletion */
 
         }
 
