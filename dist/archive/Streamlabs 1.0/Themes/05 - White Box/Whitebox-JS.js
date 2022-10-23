731adfee1cc35b7bffdb66bd2b004b11e3504894
jhoooooo <metadotmy@gmail.com>
2022-10-23 20:27:46 +0800

    added support for Youtube live

--- dist/archive/Streamlabs 1.0/Themes/05 - White Box/Whitebox-JS.js
+++ dist/archive/Streamlabs 1.0/Themes/05 - White Box/Whitebox-JS.js
@@ -13,7 +13,7 @@ document.addEventListener('onEventReceived', function (obj) {
     const msgLimit = {msg_limit};
     const msgParent = document.querySelector('.sl__chat__layout');
 
-    if (obj.detail.command === "PRIVMSG") {
+    if (obj.detail.command === "PRIVMSG" || obj.detail.command === "youtube#liveChatMessage") {
         if (smoothscroll == true) {
             $('#log>div').last().hide().slideToggle(600, "easeInOutQuart");
         }
